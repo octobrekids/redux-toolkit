6bb09043a84fb0036470564412fd41b556198a12
octobrekids <chnrd.k@gmail.com>
2021-03-13 01:31:04 +0700

    define inuse state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import todos from './constants/Todos';
+
+const selectedTodoId = todos[1].id;
+const editedCount = 0;
 
 function App() {
+  const [newTodoInput, setNewTodoInput] = useState<string>("");
+  const [editTodoInput, setEditTodoInput] = useState<string>("");
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const editInput = useRef<HTMLInputElement>(null);
+
+
+  
+  
   return (
     <div className="App">
      
