a94a36f6ed147f32267d446ee72ac8a86f4778ea
octobrekids <chnrd.k@gmail.com>
2021-03-14 03:28:34 +0700

    add counterReducer

--- src/redux-og.ts
+++ src/redux-og.ts
@@ -152,5 +152,22 @@ const selectedTodoReducer = (
 
 }
 
-
-
+const counterReducer = (state: number = 0, action: TodoActionTypes) => {
+    switch(action.type) {
+        case CREATE_TODO: {
+            return state + 1;
+        }
+        case EDIT_TODO: {
+            return state + 1;
+        }
+        case DELETE_TODO: {
+            return state + 1;
+        }
+        case TOGGLE_TODO: {
+            return state + 1;
+        }
+        default: {
+            return state
+        }
+    }
+}
